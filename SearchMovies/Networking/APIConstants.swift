d8f5f3a0eac9cf26cf5097585cf21ad0c7d90c12
MedhatMebed <medhat.mebed@gmail.com>
2019-07-21 18:36:08 +0200

    create endpoints for APIs

--- SearchMovies/Networking/APIConstants.swift
+++ SearchMovies/Networking/APIConstants.swift
@@ -10,14 +10,20 @@ import Foundation
 import Alamofire
 
 //Client Info
-let G_CLIENT_ID = " "
-let G_BASE_URL = " "
+let G_CLIENT_ID = "fd2b04342048fa2d5f728561866ad52a"
+let G_BASE_URL = "https://api.themoviedb.org/3/"
+let imagesBaseUrl = "https://image.tmdb.org/t/p/"
+
 
 struct ApiPath {
     //POST Methods
-
+    
     //UPLOAD POST Methods
-  
+    
     //GET Methods
+    static let G_GET_ACTION_SEARCH_MOVIE = "search/movie"
+    static var G_GET_ACTION_MOVIE_DETAIL = "movie"
+    static var G_GET_MOVIE_CREDITS = "/movie/{movie_id}/credits"
+    static var G_GET_MOVIE_IMAGES = "/movie/{movie_id}/images"
+    
 }
-
--- SearchMovies/Networking/APIEndPoints.swift
+++ SearchMovies/Networking/APIEndPoints.swift
@@ -15,8 +15,6 @@ protocol ParameterBodyMaker {
 }
 
 internal enum ServicePath: ParameterBodyMaker {
-  
-    
     
     func encodeBodyEnvelop() throws -> Data? {
         do {
@@ -31,12 +29,59 @@ internal enum ServicePath: ParameterBodyMaker {
     }
     
     //MARK: - API Request Parameters
+    case GetIsPhoneExist(apiKey: String, query : String, page : Int)
+    case GetMovieDetail(apiKey : String)
+    case GetMovieCredits(apiKey : String)
+    case GetMovieImages(apiKey : String)
+    
     func httpBodyEnvelop() -> [String : Any]? {
-        return [:]
+        switch self {
+        case .GetIsPhoneExist(apiKey: let apiKey, query : let query, page : let page):
+            let getQuery = ["api_key" : apiKey,
+                            "query" : query,
+                            "page" : page] as [String : Any]
+            return getQuery
+            
+        case.GetMovieDetail(apiKey: let apiKey):
+            let getQuery = ["api_key" : apiKey]
+            return getQuery
+        case.GetMovieCredits(apiKey: let apiKey):
+            let getQuery = ["api_key" : apiKey]
+            return getQuery
+        case .GetMovieImages(apiKey: let apiKey):
+            let getQuery = ["api_key" : apiKey]
+            return getQuery
+        }
     }
 }
 
 // MARK: - API Category - Customer
-
-
+struct GetMovieSearch : Requestable {
+    var apiPath: String { return ApiPath.G_GET_ACTION_SEARCH_MOVIE }
+    var httpType: HTTPMethod { return .get }
+    var ref: ResponseParser.Type { return SearchModel.self}
+    var parametersType: REQUEST_PARAM_TYPE { return .Query }
+    var pathType: ServicePath
+}
+struct GetMovieDetail : Requestable {
+    var apiPath: String { return ApiPath.G_GET_ACTION_MOVIE_DETAIL }
+    var httpType: HTTPMethod { return .get }
+    var ref: ResponseParser.Type { return MovieDetail.self}
+    var parametersType: REQUEST_PARAM_TYPE { return .Query }
+    var pathType: ServicePath
+}
+struct GetMovieCredits: Requestable {
+    var apiPath: String { return ApiPath.G_GET_MOVIE_CREDITS }
+    var httpType: HTTPMethod { return .get }
+    var ref: ResponseParser.Type { return MovieCredits.self}
+    var parametersType: REQUEST_PARAM_TYPE { return .Query }
+    var pathType: ServicePath
+}
+struct GetMoviesImages: Requestable {
+    var apiPath: String { return ApiPath.G_GET_MOVIE_IMAGES }
+    var httpType: HTTPMethod { return .get }
+    var ref: ResponseParser.Type { return MovieImages.self}
+    var parametersType: REQUEST_PARAM_TYPE { return .Query }
+    var pathType: ServicePath
+}
 
